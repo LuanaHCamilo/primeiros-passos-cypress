c80694ffe11312b34992376d6c6fd0bd35e99a17
LuanaHCamilo <luanacamilo.contato@hotmail.com>
2025-02-10 08:55:37 +0000

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,47 +1,44 @@
 import userData from '../fixtures/users/userData.json'
+import loginPage from '../pages/loginPage' 
+import dashboardPage from '../pages/dashboardPage' 
+
+const loginPage = new loginPage()
+const dashboardPage = new dashboardPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",  
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyyy-dd-mm']",
     dateCloseButton: ".--close",
-    submitButton: "[type='submit']"
+    submitButton: ".orangehrm-left-space"
+
   }
 
   it. only('User Info Update - Success', () => {
-    
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList. passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList. loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click ()
+    loginPage.accessloginPage()
+    loginPage.loginWithAnyUser (userData.userSuccess.username, userData.userSuccess.password)
+ 
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+
     cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
     cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
     cy.get(selectorsList.genericField).eq(4).clear().type('EmployeeTest')
     cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
     cy.get(selectorsList.genericField).eq(6).clear().type('DriversLicenseTest')
     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.genericField).eq(8).clear().type('ssnNumber Test')
-    cy.get(selectorsList.genericField).eq(9).clear().type('SinNumber Test')
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain','Successfully Updated')
+    cy.get(selectorsList.submitButton).eq(1).click()
+    cy.get('body').should('contain','Success');
     cy.get('.oxd-toast-close')
     
   })
   it('login - Fail', () => {
-    cy.visit('/auth/login')
+    
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
---
+++ cypress/pages/dashboardPage.js
@@ -0,0 +1,18 @@
+class dashboardPage {
+
+    selectorsList () {
+        const selectors = {
+            dashboardGrid: ".orangehrm-dashboard-grid",
+        }
+
+        return selectors
+    }
+
+    checkdashboardPage() {
+        cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+        cy.get(this.selectorsList(). dashboardGrid).should('be.visible')
+    }
+
+}
+
+export default dashboardPage
